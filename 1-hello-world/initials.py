6b5ea10be2b365fbdf1cbefa4464b33e2e5d696b
Rumomo <ruymanmora@gmail.com>
2025-02-26 08:52:28 +0000

    tercer ejercicio

---
+++ 1-hello-world/initials.py
@@ -0,0 +1,8 @@
+# Write code below 💖
+print('RRRRRR  MM       MM')
+print('R    R  M M     M M ')
+print('R    R  M   M  M  M')
+print('RRRRRR  M    M    M')
+print('R R     M         M')
+print('R  R    M         M')
+print('R   R   M         M')
